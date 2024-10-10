a51bfce65c8d302e7f9fb83b4a4cf475db9f748e
Bujade <bujade@yahoo.com>
2024-10-10 17:27:51 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+# Title of the Dashboard
+st.title("Hello World Streamlit Dashboard")
+
+# Adding a simple text
+st.write("Welcome to your first Streamlit dashboard!")
+
+# Adding a button
+if st.button('Say hello'):
+st.write('Hello, World!')
+else:
+st.write('Goodbye, World!')
