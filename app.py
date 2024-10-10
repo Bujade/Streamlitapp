510b7b6a3b9d967968285e9fba44bd9f66a371c6
Bujade <bujade@yahoo.com>
2024-10-10 17:43:54 +0100

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ st.write("Welcome to your first Streamlit dashboard!")
 
 # Adding a button
 if st.button('Say hello'):
-st.write('Hello, World!')
+  
+    st.write('Hello, World!')
 else:
-st.write('Goodbye, World!')
+    st.write('Goodbye, World!')
